335a2dc80eac3ebd377d38d8cf870b62402c6842
ObiWanWheeler <charliestinhorto@gmail.com>
2021-02-25 18:32:43 +0000

    add routing

--- src/components/image link form/ImageLinkForm.jsx
+++ src/components/image link form/ImageLinkForm.jsx
@@ -10,7 +10,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             <div className='center'>
                 <div className='center image-form pa4 br3 shadow-5'>
                     <input className='f4 pa2 w-70 center' type="text" placeholder="Enter image url..." onChange={onInputChange}/>
-                    <button className='w-30 grow f4 link ph3 pa3 3pv2 dib white bg-light-purple' onClick={onButtonSubmit}>{'detect'}</button>
+                    <button className='w-30 grow f4 link ph3 pa3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>{'detect'}</button>
                 </div>
             </div>
         </React.Fragment>
